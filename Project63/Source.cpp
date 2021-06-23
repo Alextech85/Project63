76c97c2c295b5b6f647233e119d334742bb7009a
Студент компьютерной Академии <step_student@IT-STEP.local>
2021-06-23 21:16:24 +0300

    Добавьте файлы проекта.

---
+++ Project63/Source.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	//char str[100];
+	//char symbol;
+	//while (cin.get(symbol))
+	//	cout << symbol;
+
+	//cin >> setw(10) >> str;*/	
+
+	/*cin.getline(str, 100);	
+	cout << str << cin.gcount() << "\n";
+	
+	cin.getline(str, 100);
+	cout << str << cin.gcount() << "\n";*/
+
+		
+	//string goodstr;
+	//cin >> goodstr;	
+	//getline(cin, goodstr);
+	//cout << goodstr;
+
+	//cin.ignore();
+
+	//while(symbol = cin.peek())
+
+	//cout.setf(ios::hex, ios::basefield);
+	//cout.setf(ios::uppercase);	
+	//cout << 30;
+	//cout.unsetf(ios::hex);
+	//cout << 30 << "\n";
+
+	//bool f{ true };
+
+	//cout.setf(ios::boolalpha);//cout << ((f) ? "true" : "false");
+
+	//cout << f;
+
+	//float x{ 123.456 };
+
+	//cout.fill('*');
+	//
+	//cout << x << "\n";
+
+	//cout << setw(12) << x << "\n";
+	//cout << setw(12) << left << x << "\n";
+	//cout << setw(12) << right << x << "\n";
+	//cout << setw(12) << internal << x << "\n";
+
+
+	/*cout << fixed;
+	cout << setprecision(0) << x << "\n";
+	cout << setprecision(1) << x << "\n";
+	cout << setprecision(2) << x << "\n";
+	cout << setprecision(3) << x << "\n";
+	cout << setprecision(4) << x << "\n";
+	cout << setprecision(5) << x << "\n";
+
+	cout << "\n";
+
+	cout << scientific;
+	cout << setprecision(0) << x << "\n";
+	cout << setprecision(1) << x << "\n";
+	cout << setprecision(2) << x << "\n";
+	cout << setprecision(3) << x << "\n";
+	cout << setprecision(4) << x << "\n";
+	cout << setprecision(5) << x << "\n";
+
+	cout << "\n";
+
+	cout << fixed;
+	cout << showpoint;
+	cout << setprecision(0) << x << "\n";
+	cout << setprecision(1) << x << "\n";
+	cout << setprecision(2) << x << "\n";
+	cout << setprecision(3) << x << "\n";
+	cout << setprecision(4) << x << "\n";
+	cout << setprecision(5) << x << "\n";*/
+		
+	size_t x;
+
+	return 0;
+}
